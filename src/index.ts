0271a96cc51af288f3d2caa2e9bd22ba52ef9005
barash <assafba@wix.com>
2022-12-29 15:56:18 +0200

    feat(app-config-manager): adds app config manager

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,75 @@
-console.log('hello world!!')
+import fs, { read } from 'fs';
+import os from 'os';
+import path from 'path';
+
+type AppConfigManagerState = {
+  appName: string;
+  configName: string;
+  prefix: string;
+};
+
+export const AppConfigManager = <T extends {}>(
+  options: Partial<AppConfigManagerState> & { appName: string }
+) => {
+  const state: AppConfigManagerState = {
+    configName: 'config.json',
+    prefix: '.',
+    ...options,
+  };
+
+  let config: T;
+
+  const setup = (initialConfig?: T) => {
+    if (!isConfigDirExists()) {
+      createConfigDir();
+      createConfigFile(initialConfig);
+    }
+
+    if (!isConfigFileExists()) {
+      createConfigFile(initialConfig);
+    }
+
+    config = readConfig();
+  };
+
+  const isConfigDirExists = (): boolean => fs.existsSync(getConfigDirPath());
+  const isConfigFileExists = (): boolean => fs.existsSync(getConfigFilePath());
+
+  const getConfigDirPath = (): string =>
+    path.join(os.homedir(), `${state.prefix}${state.appName}`);
+
+  const getConfigFilePath = (): string =>
+    path.join(getConfigDirPath(), state.configName);
+
+  const createConfigDir = () => fs.mkdirSync(getConfigDirPath());
+  const createConfigFile = (initialConfig?: T) =>
+    fs.writeFileSync(getConfigFilePath(), JSON.stringify(initialConfig || {}));
+
+  const readConfig = (): T => {
+    return JSON.parse(fs.readFileSync(getConfigFilePath()).toString()) as T;
+  };
+
+  const writeConfig = (data: Partial<T>, update = true) => {
+    fs.writeFileSync(
+      getConfigFilePath(),
+      JSON.stringify({
+        ...(update ? readConfig() : {}),
+        ...data,
+      })
+    );
+    config = readConfig();
+  };
+
+  const getConfig = (): T => {
+    if (!config) config = readConfig();
+    return config;
+  };
+
+  const removeConfigValue = (key: keyof T) => {
+    let tmp = readConfig();
+    delete tmp[key];
+    writeConfig(tmp, false);
+  };
+
+  return { setup, getConfig, writeConfig, removeConfigValue };
+};
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,49 @@
-describe('should say hello', ()=>{
-    it('should say hello', ()=>{
-        expect(true).toBe(false)
-    })
-})
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import { AppConfigManager } from '../src';
+
+type TestAppConfig = {
+  someData: string;
+};
+type TestAppConfigManager = ReturnType<typeof AppConfigManager<TestAppConfig>>;
+
+describe('AppConfigManager', () => {
+  const appName = 'test-app';
+  const configDir = path.join(os.homedir(), `.${appName}/config.json`);
+  let appConfigManager: TestAppConfigManager;
+
+  beforeEach(() => {
+    appConfigManager = AppConfigManager<TestAppConfig>({ appName });
+  });
+
+  afterEach(() => {
+    fs.rmSync(configDir, { recursive: true, force: true });
+  });
+
+  it('should setup app config dir on user home dir', () => {
+    expect(fs.existsSync(configDir)).toBe(false);
+    appConfigManager.setup();
+    expect(fs.existsSync(configDir)).toBe(true);
+  });
+
+  it('should read default config correctly', () => {
+    appConfigManager.setup();
+    expect(appConfigManager.getConfig()).toEqual({});
+  });
+
+  it('should set & retrieve config correctly', () => {
+    const expectedResult = { someData: '1234' };
+    appConfigManager.setup();
+
+    appConfigManager.writeConfig(expectedResult);
+    expect(appConfigManager.getConfig()).toEqual(expectedResult);
+  });
+
+  it('should setup config with data', () => {
+    appConfigManager.setup({ someData: '1234' });
+
+    appConfigManager.removeConfigValue('someData');
+    expect(appConfigManager.getConfig()).toEqual({});
+  });
+});
